ed1a68c29931d261712677d63240f0e5e4cc6f79
Sheila Nyambura <sheilanyambura394@gmail.com>
2024-02-02 10:25:59 +0300

    updated app.py file

--- python-code-challenge-superheroes/code-challenge/app/app.py
+++ python-code-challenge-superheroes/code-challenge/app/app.py
@@ -4,11 +4,15 @@ from flask_migrate import Migrate
 from flask_restful import Api,Resource
 from werkzeug.exceptions import NotFound
 # from sqlalchemy.ext.declarative import declarative_base
+
 from models import db, Hero, Power, HeroPower
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # base = declarative_base()
+
+
 migrate = Migrate(app, db)
 
 db.init_app(app)
@@ -49,7 +53,8 @@ api.add_resource(HeroesByID, '/heroes/<int:id>')
 class Powers(Resource):
     def get(self):
         response_dict= [n.to_dict() for n in Power.query.all()]
-        
+        # response_dict= [n.serialize() for n in Power.query.all()]
+
         response= make_response(response_dict, 200)
         return  response
 
@@ -71,43 +76,74 @@ class PowersByID(Resource):
     def patch(self,id):
         power = Power.query.filter_by(id=id).first()
         record_dict= power.to_dict() if power else None
-        
-        if record_dict== None:
-            error_dict=  {'error': 'Power not found'}
-            response= make_response(error_dict, 404)
-            return response
-        else:
-            for attr in request.form:
-                setattr(power, attr, request.form[attr])
+        try:
+            if record_dict== None:
+                error_dict=  {'error': 'Power not found'}
+                response= make_response(error_dict, 404)
+                return response
+            else:
+                for attr in request.form:
+                    setattr(power, attr, request.form[attr])
+                
+                db.session.add(power)
             
-            db.session.add(power)
-            try:
                 db.session.commit()
                 power_dict= power.to_dict()
                 response= make_response(power_dict, 200)
                 return response
-            except:
-                err_dict= {"errors" : ["validation errors"]}
-                response= make_response(err_dict, 404)
-                db.session.rollback()
-                return response         
+        except:
+            err_dict= {"errors" : ["validation errors"]}
+            response= make_response(err_dict, 404)
+            db.session.rollback()
+            return response         
 
 api.add_resource(PowersByID, '/powers/<int:id>')
 
 class HeroPowers(Resource):
+
+    def get(self):
+        hero_power_dict = [r.to_dict() for r in HeroPower.query.all()] 
+        if hero_power_dict:
+            response= make_response(jsonify(hero_power_dict), 200)
+            return response
+        else:
+            error_dict=  {'error': 'Hero power not found'}
+            response= make_response(error_dict, 404)
+            return response
+
     def post(self):
-        new_record=  HeroPower(
-            strength= request.form['strength'],
-            power_id=  int(request.form['power_id']),
-            hero_id=   int(request.form['hero_id']),
-        )
-        db.session.add(new_record)
-        db.session.commit()
-        
-        response_dict= new_record.to_dict()
-        
-        response= make_response(jsonify(response_dict), 201)
-        return response
+        try:
+            # We are using this to allow us post JSON data from a react form.
+            data= request.get_json() 
+            
+            # # strength= request.form['strength']
+            # power_id=  int(request.form['power_id'])
+            # hero_id=   int(request.form['hero_id'])
+            strength= data.get("strength")
+            power_id= data.get('power_id')
+            hero_id=  data.get('hero_id')
+            # )
+            #Checking if power and hero exist
+            power= Power.query.get(power_id)
+            hero= Hero.query.get(hero_id)
+
+            if not (power and hero):
+                return make_response(jsonify({"errors": ["Power  or Hero does not exist."]}), 404)
+            
+            #Creating new hero power
+            new_record= HeroPower(strength=strength, power_id=power_id, hero_id=hero_id)
+            db.session.add(new_record)
+            db.session.commit()
+            
+            hero_dict= hero.to_dict()
+
+            response= make_response(jsonify(hero_dict), 201)
+            return response
+        except:
+            err_dict= {"errors" : ["validation errors"]}
+            response= make_response(err_dict, 404)
+            db.session.rollback()
+            return response  
     
 api.add_resource(HeroPowers, '/hero_powers')   
      
